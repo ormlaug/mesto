30132dbff35ec75619e9f5efcc621f0bbf4ebe72
Margarita Krok <gretchen_k@mail.ru>
2022-04-19 22:54:49 +0300

    добавлены карточки на страницу с помощью js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,52 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardContainer = document.querySelector('.cards__list');
+const template = document.querySelector('.template');
+
+function render() {
+  const html = initialCards.map(getElement);
+  cardContainer.append(...html);
+}
+
+function getElement(item) {
+  const getElementTemplate = template.content.cloneNode(true);
+  const cardTitle = getElementTemplate.querySelector('.cards__title');
+  const cardPhoto = getElementTemplate.querySelector('.cards__photo');
+  cardTitle.textContent = item.name;
+  cardPhoto.src = item.link;
+  return getElementTemplate;
+}
+
+render();
+
+
 let editInfoButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup_type_edit');
 let popupCloseButton = popup.querySelector('.popup__close');
 let nameOriginal = document.querySelector('.profile__name');
 let jobOriginal = document.querySelector('.profile__text');
