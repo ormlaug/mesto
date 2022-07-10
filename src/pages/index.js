9cf19a1263025c769a3aa0df58cfa95e39d09ce7
Margarita Krok <gretchen_k@mail.ru>
2022-07-10 23:42:28 +0300

    webpack сборка

---
+++ src/pages/index.js
@@ -0,0 +1,65 @@
+import '../pages/index.css';
+
+import Section from "../components/Section.js";
+import { initialCards } from "../components/initial-cards.js";
+import Card from "../components/Card.js";
+import popupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import { 
+  cardAddButton,
+  infoEditButton,
+  nameInput,
+  jobInput,
+  config
+ } from '../utils/utils.js';
+ import { FormValidator } from "../components/FormValidator.js";
+
+ const cardFormValidator = new FormValidator(config,'.popup_type_add');
+cardFormValidator.enableValidation();
+
+const infoFormValidator = new FormValidator(config, '.popup_type_edit');
+infoFormValidator.enableValidation();
+
+const cardList = new Section({ 
+  items: initialCards, 
+  renderer: (item) => {
+    const card = new Card(item, '.template', () => popupWithPicture.open({name: item.name, link: item.link}));
+    return card.generateCard();
+  },
+},
+'.cards__list');
+
+const popupWithPicture = new popupWithImage('.popup_type_picture');
+
+const popupWithAddCardForm = new PopupWithForm('.popup_type_add', (item) => {
+  cardList.addItem(item);
+  popupWithAddCardForm.close();
+});
+
+const userID = new UserInfo('.profile__name', '.profile__text');
+
+const popupWithEditInfoForm = new PopupWithForm('.popup_type_edit', (item) => {
+  userID.setUserInfo(item);
+  popupWithEditInfoForm.close();
+});
+
+cardAddButton.addEventListener('click', function() {
+  popupWithAddCardForm.open();
+});
+
+infoEditButton.addEventListener('click', function() {
+  const userData = userID.getUserInfo();
+  nameInput.value = userData.name;
+  jobInput.value = userData.job;
+  popupWithEditInfoForm.open();
+});
+
+popupWithPicture.setEventListeners();
+popupWithAddCardForm.setEventListeners();
+popupWithEditInfoForm.setEventListeners();
+
+cardList.renderItems();
+
+
+
