53d1e035952e0441bf1e3b75b156372c67d60139
Margarita Krok <mrgrkk@gmail.com>
2022-07-19 09:18:07 +0300

    добавлены методы получения карточек и пользователя

--- src/components/Api.js
+++ src/components/Api.js
@@ -5,8 +5,36 @@ export default class Api {
     }
   
     getInitialCards() {
-      // ...
+      return fetch(`${this._url}/cards`, {
+        method: 'GET',
+        headers: this._headers
+      })
+      .then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
     }
   
     // другие методы работы с API
+    setUserInfo(data) {
+      return fetch(`${this._url}/users/me`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: data.name;
+          about = data.about;
+        })
+      })
+      .then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      })
+    }
+
+
+
   }
